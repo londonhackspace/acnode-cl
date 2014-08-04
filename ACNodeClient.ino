2ef2e77a2292d17dc40668350287a0a1c52e5188
Jasper Wallace <jasper@pointless.net>
2014-08-04 20:34:14 +0100

    now does long uid's

--- ACNodeClient.ino
+++ ACNodeClient.ino
@@ -1,3 +1,4 @@
+
 /*
 ACNodeClient
  v0.1
@@ -16,16 +17,20 @@ char serverName[] = "babbage.lan.london.hackspace.org.uk";
 int chipSelectPin = 42;
 int NRSTPD = 13;
 Mfrc522 Mfrc522(chipSelectPin, NRSTPD);
-unsigned char serNum[7];
-
+unsigned char serNum[5];
+unsigned char serNum7[8];
+enum cardTypes { CARDUID4, CARDUID7, NOCARD };
+cardTypes cardType = NOCARD;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("\n\nACNode Client Startup");
   // start the Ethernet connection:
+
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
+    // ^-- err, lol?
     while(true);
   }
   // print your local IP address:
@@ -36,14 +41,22 @@ void setup() {
     Serial.print(".");
   }
   Serial.println();
-  SPI.setModule(2);
-  pinMode(chipSelectPin, OUTPUT); //moved this here from Mfrc522.cpp
-  digitalWrite(chipSelectPin, LOW);
-  pinMode(NRSTPD, OUTPUT); //moved this here from Mfrc522.cpp
-  digitalWrite(NRSTPD, HIGH); //moved this here from Mfrc522.cpp
+
   pinMode(GREEN_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
+
+  digitalWrite(RED_LED, HIGH);
+  digitalWrite(GREEN_LED, LOW);
+
   Serial.println("Initialising Mfrc522");
+
+  SPI.setModule(2);
+
+  pinMode(chipSelectPin, OUTPUT);
+  digitalWrite(chipSelectPin, LOW);
+  pinMode(NRSTPD, OUTPUT);   
+  digitalWrite(NRSTPD, HIGH);
+
   Mfrc522.Init();
 }
 
@@ -51,51 +64,107 @@ void readcard()
 {
   unsigned char i,tmp;
   unsigned char status;
-  unsigned char str[MAX_LEN];
+  unsigned char temp[MAX_LEN];
   unsigned char RC_size;
   unsigned char blockAddr;
   String mynum = "";
 
+  temp[1] = 0;
+  temp[2] = 0;
 
-  status = Mfrc522.Request(PICC_REQIDL, str);
-  status = Mfrc522.Anticoll(str);
-  memcpy(serNum, str, 7);
+  status = Mfrc522.Request(PICC_REQIDL, temp);
+
+  if (status != MI_OK)
+  {
+    cardType = NOCARD;
+    return;
+  }
+  Serial.print("Card detected. ATQA:");
+  dumpHex((char*)temp, 2);
+  Serial.println("");
+
+  status = Mfrc522.Anticoll(temp);
   if (status == MI_OK)
   {
-    digitalWrite(GREEN_LED, HIGH);
-    delay(1000);
-    digitalWrite(GREEN_LED, LOW);
-    Serial.println("The card's number is  : ");
-    Serial.print(serNum[0], HEX);
-    Serial.print(" , ");
-    Serial.print(serNum[1], HEX);
-    Serial.print(" , ");
-    Serial.print(serNum[2], HEX);
-    Serial.print(" , ");
-    Serial.print(serNum[3], HEX);
-    Serial.print(" , ");
-    Serial.print(serNum[4], HEX);
-    Serial.print(" , ");
-    Serial.print(serNum[5], HEX);
-    Serial.print(" , ");
-    Serial.print(serNum[6], HEX);
-    Serial.print(" , ");
-  }
+    memcpy(serNum, temp, 5);
+    unsigned char sak = 0;
 
+    // status here is actually the size of something.
+    status = Mfrc522.SelectTag(serNum, &sak);
+
+    if ((sak & 0x04) == 0x00)
+    {
+      //      Serial.print(" UID: ");
+      dumpHex((char*)serNum, 4);
+      Serial.println("");
+      cardType = CARDUID4;
+      
+      if ((sak & 0x20) == 0x20)
+      {
+        //ISO/IEC FCD 14443-3: Table 9 — Coding of SAK
+        Serial.println(" UID complete, PICC compliant with ISO/IEC 14443-4");
+        //send RATS (Request for Answer To Select)
+
+        unsigned char ats[MAX_LEN];
+        unsigned int unLen = 0;
+        status = Mfrc522.RATS(ats, &unLen);
+
+        if (status == MI_OK)
+        {
+          Serial.println(" ATS: ");
+          dumpHex((char*)ats, ats[0]);
+          Serial.println("");
+        }
+      }
+      /*      Serial.print(" SAK: ");
+       Serial.print(sak, HEX);
+       Serial.println("");*/
+      return;
+    }
+    else
+    {
+      //cascading level 2
+      memcpy(serNum7, &serNum[1], 3);//cascading L1
+      status = Mfrc522.Anticoll2(temp);
+      if (status == MI_OK)
+      {
+        memcpy(&serNum7[3], temp, 4);
+        status = Mfrc522.SelectTag2(&serNum7[3], &sak);
+        //        Serial.print(" UID: ");
+        dumpHex((char*)serNum7, 7);
+        Serial.println("");
+
+        cardType = CARDUID7;
+
+        /*        Serial.print(" SAK: ");
+         Serial.print(sak, HEX);
+         Serial.println("");*/
+        return;
+      }
+      else
+      {
+        Serial.println("ANTICOLL error: cascading level 2");
+      }
+    }
+  }
 
   Mfrc522.Halt();       
 }
+
 
 void querycard()
 {
   Serial.print("Connecting to http://");
   Serial.println(serverName);
+
   if (client.connect(serverName, 1234)) {
     Serial.println("Connected");
     Serial.println("Querying");
     client.println("GET /1/card/0477861A402480");
     client.println();
+
     delay(50);
+
     if (client.available()) {
       Serial.println("Got Response:\n");
       char c = client.read();
@@ -108,16 +177,38 @@ void querycard()
 
   }
   else {
-    // kf you didn't get a connection to the server:
+    // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
-
 }
 
 void loop(){
+  digitalWrite(RED_LED, HIGH);
   readcard();
-  delay(2000);
+  if (cardType != NOCARD)
+  {
+    digitalWrite(GREEN_LED, HIGH);
+    querycard();
+  }
+  delay(500);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  delay(500);
 }
+
+
+
+void dumpHex(char* buffer, int len)
+{
+  for(byte i=0; i < len; i++) {
+    char text[4];
+    sprintf(text, "%02X\x00", (byte)(*(buffer + i)));
+    Serial.print(text);
+
+  }
+  //Serial.println(" ");
+}
+
 
 
 
