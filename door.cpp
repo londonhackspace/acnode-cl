9f6fdb0611c40a3668ae37ebc0db93d8557929fe
Jan Szumiec <jan.szumiec@gmail.com>
2015-08-28 00:09:32 +0100

    Door class for opening and closing the door.

---
+++ door.cpp
@@ -0,0 +1,33 @@
+#include "door.h"
+
+// Hints for the linker
+int Door::_pin;
+unsigned long Door::_opened_at;
+
+Door::Door(int pin) {
+  _pin = pin;
+  _opened_at = 0;
+  digitalWrite(_pin, LOW);
+}
+
+void Door::open() {
+  digitalWrite(_pin, HIGH);
+  _opened_at = millis();
+}
+
+boolean Door::opened() {
+  return _opened_at > 0;
+}
+
+void Door::close() {
+  digitalWrite(_pin, LOW);
+  _opened_at = 0;
+}
+
+boolean Door::maybe_close() {
+  if (millis() - _opened_at > 1500) {
+    close();
+    return true;
+  }
+  return false;
+}
