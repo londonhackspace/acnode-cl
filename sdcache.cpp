3a2dae1964e45c5a758a352acf5cca50fe2a4c95
Jasper Wallace <jasper@pointless.net>
2015-09-27 05:23:01 +0100

    Fix some stuff in sdcache:
    * APPEND really only appends, so don't use it
    * in WRITE mode the file position starts at the end, so seek to the beginning
    * seek() takes a uint32_t, so trying to seek to a negative offset does not work
    * the first entry could be invalid
    * fix off by one in compare()

--- sdcache.cpp
+++ sdcache.cpp
@@ -44,9 +44,14 @@ Card SDCache::get(Card u) {
 }
 
 void SDCache::set(const Card u) {
-  File f = SD.open(_filename, FILE_APPEND);
+  File f = SD.open(_filename, FILE_WRITEREAD);
   user user_entry, nu;
-  uint32_t spare = 0; // if we pass an invalid entry keep track of where it was so we can save there.
+  int spare = -1; // if we pass an invalid entry keep track of where it was so we can save there.
+  boolean add_needed = true;
+
+  // writeing starts with the position at the end of the file, so go back
+  // to the beginning.
+  f.seek(0);
 
   memset(nu.uid, 0, 7);
   u.get_uid(nu.uid);
@@ -54,26 +59,40 @@ void SDCache::set(const Card u) {
   nu.status = u.is_user();
   nu.maintainer = u.is_maintainer();
   nu.uidlen = u.get_longuid();
+  nu.invalid = 0;
 
   while (f.available()) {
     f.read(&user_entry, sizeof(struct user));
+
     if (!user_entry.invalid) {
       if (user_entry.uidlen == nu.uidlen && compare(user_entry.uid, nu.uid)) {
-        f.seek(-sizeof(struct user));
+        // do we need to update the user?
+        if (user_entry.maintainer != nu.maintainer || user_entry.status != nu.status) {
+          // yes, so update it.
+          Serial.println("Updating user in the cache");
+          f.seek(f.position() - sizeof(struct user));
+          f.write((uint8_t *)&nu, sizeof(struct user));
+        }
+        // and we don't need to do anything else
+        add_needed = false;
         break;
       }
     }
     // if we find an invalid entry keep it for later so we
     // can overwrite it.
-    if (user_entry.invalid && spare == 0) {
+    if (user_entry.invalid && spare == -1) {
       spare = f.position();
       spare -= sizeof(struct user);
     }
   }
-  if (spare > 0) {
+  if (spare >= 0) {
     f.seek(spare);
   }
-  f.write((uint8_t *)&nu, sizeof(struct user));
+  if (add_needed) {
+    Serial.print("adding needed, writing at ");
+    Serial.println(f.position());
+    f.write((uint8_t *)&nu, sizeof(struct user));
+  }
   f.close();
 }
 
@@ -99,32 +118,52 @@ int SDCache::each(void(*callback)(Card u)) {
 }
 
 boolean SDCache::compare(const uint8_t *k1, const uint8_t *k2) {
-  int i = 0;
-  while (i++ < 7) {
+  int i;
+  for (i = 0; i < 7 ; i++) {
     if (k1[i] != k2[i]) return false;
   }
   return true;
 }
 
 void SDCache::verify(void) {
-  File f = SD.open(_filename, FILE_READ);
+  File f = SD.open(_filename, FILE_WRITEREAD);
   int counter = 0;
+  int status;
+  boolean update = false;
 
   user u;
 
+  f.seek(0);
+
   while (f.available()) {
     f.read(&u, sizeof(struct user));
+    update = false;
+
     wdog.feed();
     if (!u.invalid) {
       Card t(u.uid, u.uidlen, u.status, u.maintainer);
-      if (querycard(t) == 0) {
+      status = querycard(t);
+      if (status == 0) {
         Serial.println("User no-longer valid: ");
         t.dump();
         u.invalid = 1;
-        f.seek(-sizeof(struct user));
-        f.write((uint8_t *)&u, sizeof(struct user));
+        update = true;
+      } else if (status == 2 && !u.maintainer) {
+        Serial.println("making user a maintainer");
+        u.maintainer = 1;
+        update = true;
+        counter++;
+      } else if (status == 1 && u.maintainer) {
+        Serial.println("downgrading to a user");
+        u.maintainer = 0;
+        update = true;
+        counter++;
       } else {
         counter++;
+      }
+      if (update) {
+        f.seek(f.position() - sizeof(struct user));
+        f.write((uint8_t *)&u, sizeof(struct user));
       }
     }
   }
