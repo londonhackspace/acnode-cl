81fa56a69489749e8a55290d08f582fe9662cd2c
Jan Szumiec <jan.szumiec@gmail.com>
2016-07-10 11:43:42 +0100

    ACNode extraction.

--- src/acnode.h
+++ src/acnode.h
@@ -7,6 +7,13 @@
 #include "card.h"
 #include "watchdog.h"
 #include "cache.h"
+#include "every.h"
+#include "rgb.h"
+#include "role.h"
+#include "tool.h"
+#include "microrl.h"
+#include "network.h"
+#include "button.h"
 
 // put these things here so more things can use it.
 extern settings acsettings;
@@ -30,5 +37,46 @@ extern Cache *cache;
 
 #define TRACE Serial.print(__FILE__); Serial.print(": "); Serial.println( __LINE__);
 
-#endif
+enum MenuType {NOMENU, USER, MAINTAINER};
+enum MenuState {ADD, OFFLINE, ONLINE};
 
+class ACNode : public Role {
+public:
+  ACNode(PN532 &, RGB &, Tool &, Button &b, microrl_t *);
+  void run();
+protected:
+  void card_loop();
+  void menu_loop();
+  void offline();
+  void online();
+  void user_menu(int);
+  void maintainer_menu(int);
+  Card readcard();
+private:
+  Every card_every;
+  Every five_sec;
+  Every heart_every;
+  Every one_sec;
+  Every tool_status_check;
+
+  bool heartbeat;
+  bool card_on_reader;
+  bool adding;
+  unsigned long adding_timeout;
+  unsigned long menu_timeout;
+
+  MenuType menu;
+  MenuState menu_state;
+
+  RGB &rgb;
+  Tool &tool;
+  Button &button;
+
+  microrl_t *prl;
+
+  Card cu;
+  Card cc;
+  Card maintainer;
+};
+
+#endif
