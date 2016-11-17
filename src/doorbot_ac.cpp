0449aae7e968ed75984923e7352e7636a22f7497
Jan Szumiec <jan.szumiec@gmail.com>
2016-11-17 22:14:21 +0000

    Tiny fix.

--- src/doorbot_ac.cpp
+++ src/doorbot_ac.cpp
@@ -18,7 +18,7 @@ void DoorbotWithAccessControl::handleCardPresent(Card c) {
   int status;
   Card cached = cache->get(c);
   if (cached.compare_uid(c) && cached.is_valid()) {
-    status = (cached.is_user() || cached.is_maintainer()) ? 1 : 0;
+    status = 1;
   } else {
     status = networking::querycard(c);
   }
