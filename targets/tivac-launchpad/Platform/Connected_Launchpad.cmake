2d6a52baff2203146c80a734784952df3a8c4524
Michael Cullen <michael@cullen-online.com>
2019-08-21 00:10:34 +0100

    Fixed up compiler options to show warnings for some parts of the code
    For now, continuing to hide them in the library code.

--- targets/tivac-launchpad/Platform/Connected_Launchpad.cmake
+++ targets/tivac-launchpad/Platform/Connected_Launchpad.cmake
@@ -1,8 +1,8 @@
 
-set(COMMON_FLAGS "-g -Os -w -ffunction-sections -fdata-sections -nostdlib -fno-threadsafe-statics --param max-inline-insns-single=500 -fno-rtti -fno-exceptions -Dprintf=iprintf -MMD -mcpu=cortex-m4 -mthumb -DF_CPU=120000000L -DARDUINO=10610 -DENERGIA=10610 -DENERGIA_EK_TM4C1294XL -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mabi=aapcs")
+set(COMMON_FLAGS "-g -Os -ffunction-sections -fdata-sections -nostdlib -fno-threadsafe-statics --param max-inline-insns-single=500 -Dprintf=iprintf -MMD -mcpu=cortex-m4 -mthumb -DF_CPU=120000000L -DARDUINO=10610 -DENERGIA=10610 -DENERGIA_EK_TM4C1294XL -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -mabi=aapcs")
 
 set(CMAKE_C_FLAGS "${COMMON_FLAGS}" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "${COMMON_FLAGS}"  CACHE STRING "")
+set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -fno-rtti -fno-exceptions"  CACHE STRING "")
 set(CMAKE_EXE_LINKER_FLAGS "-nostartfiles -nostdlib -T ${LINKER_SCRIPT} -Wl,--check-sections -Wl,--gc-sections -Wl,--entry=ResetISR -Wl,--unresolved-symbols=report-all -Wl,--warn-common -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant -Wl,--start-group -Wl,-Map,${CMAKE_PROJECT_NAME}.map -lc -lm -lgcc" CACHE STRING "")
 
 set(ENERGIA_CORE ${CMAKE_SOURCE_DIR}/targets/tivac-launchpad/tivac-core)
