74e1d695402b5c90104e5411955d7645171f8668
JonHodgson <j.r.hodgson@gmail.com>
2024-12-09 23:33:30 +0000

    Fixed previous macro definition warning

--- src/macros.h
+++ src/macros.h
@@ -1,4 +1,7 @@
 // A bunch of useful macros.
 
 #define NOTIMPLEMENTED  Serial.println(String(__FILE__) + ":" + String(__LINE__) + ": Not implemented.");
+#ifdef DEBUG
+#  undef DEBUG
+#endif
 #define DEBUG(x)        Serial.println(String(__FILE__) + ":" + String(__LINE__) + ":" + x);
